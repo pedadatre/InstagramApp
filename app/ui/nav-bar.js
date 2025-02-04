ec524c8158e617056a5415b092b32503c8e467de
gerard <gerard>
2025-02-04 18:13:47 +0000

    .

--- app/ui/nav-bar.js
+++ app/ui/nav-bar.js
@@ -1,3 +1,5 @@
+'use client'
+
 import NavLink from '@/app/ui/nav-link'
 import { HomeIcon, MagnifyingGlassIcon, PlusCircleIcon, UserIcon } 
 from '@heroicons/react/24/solid'
@@ -9,10 +11,10 @@ export default () => {
         <nav className="flex flex-col gap-4 h-dvh border-e p-2" >
             <p className='hidden sm:block'>SocialApp</p>
             
-            <NavLink ruta="/" texto="Home" icono={<HomeIcon />}></NavLink>
-            <NavLink ruta="/search" texto="Search" icono={<MagnifyingGlassIcon />}></NavLink>
-            <NavLink ruta="/create" texto="Create" icono={<PlusCircleIcon />}></NavLink>
-            <NavLink ruta="/profile" texto="Profile" icono={<UserIcon />}></NavLink>
+            <NavLink ruta="/" texto="Home" icon={ HomeIcon }></NavLink>
+            <NavLink ruta="/search" texto="Search" icon={MagnifyingGlassIcon}></NavLink>
+            <NavLink ruta="/create" texto="Create" icon={PlusCircleIcon}></NavLink>
+            <NavLink ruta="/profile" texto="Profile" icon={UserIcon}></NavLink>
         </nav>
     )
 
--- app/ui/nav-link.js
+++ app/ui/nav-link.js
@@ -4,9 +4,9 @@ import { BeakerIcon, HomeIcon } from '@heroicons/react/24/solid'
 import clsx from "clsx"
 const { usePathname } = require("next/navigation");
 
-export default ({ruta, texto, icono}) => {
+export default ({ruta, texto, icon}) => {
 
-    //const objeto = { icono };
+    const objeto = { icon };
 
     const path = usePathname();
 
@@ -18,7 +18,7 @@ export default ({ruta, texto, icono}) => {
                 "font-bold pointer-events-none": path === ruta
             })}
         >
-            <HomeIcon className="w-4" />
+            <objeto.icon className="w-4" />
             <span className="hidden sm:inline">{texto}</span>
         </Link>
     )
